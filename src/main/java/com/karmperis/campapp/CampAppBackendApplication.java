1af9141bb26d903e958e3f8eb797fd6c86918e22
Nikolaos Karmperis <karmperis@gmail.com>
2026-08-25 17:20:05 +0300

    chore: add initial Spring Boot project structure

---
+++ src/main/java/com/karmperis/campapp/CampAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.karmperis.campapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karmperis/campapp/CampAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karmperis.campapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
