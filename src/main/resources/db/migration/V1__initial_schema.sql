15fabb282481397a5d822d35f7d73e1fcf8f2fc7
Nikolaos Karmperis <karmperis@gmail.com>
2026-09-12 10:21:14 +0300

    feat: add webhook events table

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -944,3 +944,15 @@ CREATE TABLE refunds
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE webhook_events
+(
+    event_id VARCHAR(255) COLLATE utf8mb4_0900_bin NOT NULL,
+    event_type VARCHAR (100) NOT NULL,
+    processed_at DATETIME(6) NOT NULL,
+
+    CONSTRAINT pk_webhook_events PRIMARY KEY (event_id)
+
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
