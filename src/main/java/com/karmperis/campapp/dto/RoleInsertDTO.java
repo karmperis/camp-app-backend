c90df1763e05f4e45762b02652f04bdb73a999f0
Nikolaos Karmperis <karmperis@gmail.com>
2026-09-23 20:51:23 +0300

    style: add blank line for readability in RoleInsertDTO

--- src/main/java/com/karmperis/campapp/dto/RoleInsertDTO.java
+++ src/main/java/com/karmperis/campapp/dto/RoleInsertDTO.java
@@ -12,6 +12,7 @@ import jakarta.validation.constraints.Size;
 
 @GroupSequence({RoleInsertDTO.First.class, RoleInsertDTO.Second.class, RoleInsertDTO.class})
 public record RoleInsertDTO(
+
         @NotBlank(message = "The role name cannot be blank.", groups = First.class)
         @Size(min = 4, max = 50, message = "The role name must contain between 4 and 50 characters.", groups = Second.class)
         String name
